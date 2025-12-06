a4a7e322ff75212d050a582cf443fa4b016451eb
Vordgi <sasha2822222@gmail.com>
2025-12-06 19:48:05 +0000

    nic-48 update default logger message format

--- packages/cache-redis/src/lib/logger.ts
+++ packages/cache-redis/src/lib/logger.ts
@@ -1,7 +1,5 @@
 import { type LogData } from "../types";
 
 export const logger = (logData: LogData) => {
-    console.log(
-        `> Cache | ${logData.type}\n\tKey: "${logData.key}"\n\tStatus: ${logData.status}\n\tSource: ${logData.source}`,
-    );
+    console.log(`[Cache ${logData.type}] ${logData.status} from ${logData.source} | ${logData.key}`);
 };
