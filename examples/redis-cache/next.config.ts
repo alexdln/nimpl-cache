e915a9b14a3ff74ada0b081a564cbf469d2ea2bb
Vordgi <sasha2822222@gmail.com>
2025-12-08 10:43:01 +0000

    nic-59 add connection strategy to example

--- examples/redis-cache/next.config.ts
+++ examples/redis-cache/next.config.ts
@@ -2,11 +2,11 @@ import { type NextConfig } from "next/types";
 
 const nextConfig: NextConfig = {
     cacheComponents: true,
-
     cacheHandlers: {
-        default: import.meta.resolve("@nimpl/cache-redis"),
-        // default: import.meta.resolve("./cache-handler.js"),
+        // default: import.meta.resolve("@nimpl/cache-redis"),
+        default: import.meta.resolve("./cache-handler.js"),
     },
+    cacheMaxMemorySize: 0,
 };
 
 export default nextConfig;
