93a8337e642f9a12c0cf62474e52731014040f7e
Vordgi <sasha2822222@gmail.com>
2025-12-14 14:53:11 +0000

    nic-107 update example

--- examples/redis-cache/src/app/page.tsx
+++ examples/redis-cache/src/app/page.tsx
@@ -4,7 +4,7 @@ import { ClientTime } from "./client-time";
 
 export default async function Home() {
     "use cache";
-    cacheLife({ stale: 5, revalidate: 10, expire: 10000 });
+    cacheLife({ stale: 30, revalidate: 60, expire: 300 });
 
     const revalidateStateDate = new Date();
     await new Promise((resolve) => setTimeout(resolve, 5000));
